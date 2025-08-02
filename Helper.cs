7ac052365daf4146b1b9167506d0e37899b830ef
Anjel28 <anjelchaudhary28@gmail.com>
2025-08-02 23:08:07 +0530

    Add files via upload

---
+++ Helper.cs
@@ -0,0 +1,12 @@
+using System;
+
+namespace BuildDemo
+{
+    class Helper
+    {
+        public static void PrintMessage()
+        {
+            Console.WriteLine("Helper method called!");
+        }
+    }
+}
